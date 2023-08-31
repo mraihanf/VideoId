3e9d2c7487a145bb32d1b022077f906639a6f7b4
Muhammad Raihan Fadhlullah <fadhlullah24@gmail.com>
2023-08-31 13:08:50 +0700

    FIRST COMMIT

---
+++ src/main/java/ID/VideoId/VideoIdApplication.java
@@ -0,0 +1,13 @@
+package ID.VideoId;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoIdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ID/VideoId/VideoIdApplicationTests.java
@@ -0,0 +1,13 @@
+package ID.VideoId;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
